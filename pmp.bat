b77773a882fd194a9ca90c2ef459eb209955c68c
c64pectre <113802295+c64pectre@users.noreply.github.com>
2023-10-07 07:18:36 +0200

    Renamed ibn-handler/ibn-handler.drv to irq-brk-nmi-handler/irq-brk-nmi.drv because more clear.

--- pmp.bat
+++ pmp.bat
@@ -2,7 +2,7 @@ set PMP_PROJECT_GROUP_ID=org.c64pectre
 set PMP_PROJECT_ARTIFACT_ID=os64
 set PMP_PROJECT_ARTIFACT_VERSION=1.0.0-SNAPSHOT
 set PMP_PROJECT_PACKAGING=d64
-set PMP_PROJECT_MODULES=boot load u64-charset sprites ibn-handler kernal screen test-screen
+set PMP_PROJECT_MODULES=boot load u64-charset sprites irq-brk-nmi-handler kernal screen test-screen
 rem boot must be the first module in order for autostart to work.
 
 set PMP_PACKAGER=d64
